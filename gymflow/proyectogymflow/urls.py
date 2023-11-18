12ec0d1314ffb8212b378df2da0979cef55906cc
Gatute <145714792+Gatute@users.noreply.github.com>
2023-11-18 18:24:03 -0300

    Add files via upload

---
+++ gymflow/proyectogymflow/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from .views import login, inicio, perfil, base , registro, lista_usuarios, user_logeo, rutina
+
+urlpatterns = [
+    path('gimnasio/logeo', user_logeo, name="logein"), # Funcion para logearse
+    path('', login, name="gimnasio"),
+    path('gimnasio/inicio', inicio, name="inicio"),
+    path('gimnasio/perfil', perfil, name='perfil'),
+    path('gimnasio/base', base, name='usuarios'),
+    path('gimnasio/registro', registro, name='registro'),
+    path('gimnasio/usuarios', lista_usuarios, name='lista_usuarios'),
+    path('gimnasio/rutina', rutina, name='rutina')
+] 
