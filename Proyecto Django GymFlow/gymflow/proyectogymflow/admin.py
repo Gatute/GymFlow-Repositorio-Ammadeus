4b3d3f0fa38441f492432a362a484ecf585a7030
Gatute <145714792+Gatute@users.noreply.github.com>
2023-11-21 10:10:59 -0300

    Add files via upload

---
+++ Proyecto Django GymFlow/gymflow/proyectogymflow/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import user_list, perfil_usuario_login
+
+# Register your models here.
+
+admin.site.register(user_list)
+admin.site.register(perfil_usuario_login)
